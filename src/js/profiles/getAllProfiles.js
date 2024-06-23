d84634cbdfb45baa211a85ea3c2431be955ef059
EF-Pami <pamijoelpami@gmail.com>
2024-06-23 21:15:27 +0200

    added getProfiles.js

---
+++ src/js/profiles/getAllProfiles.js
@@ -0,0 +1,45 @@
+import { BASE_URL, PROFILE_ENDPOINT } from "../baseapi.js";
+
+export async function fetchProfiles() {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    const apiKey = localStorage.getItem("api_key");
+    let currentPage = 1;
+
+    async function fetchPage(page) {
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Noroff-API-Key": apiKey,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const response = await fetch(
+        `${BASE_URL}${PROFILE_ENDPOINT}?page=${page}`,
+        options,
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        if (!data.meta.isLastPage) {
+          await fetchPage(page + 1);
+        }
+      } else {
+        alert(
+          "Error fetching profiles - Page",
+          page,
+          ":",
+          response.status,
+          response.statusText,
+        );
+      }
+    }
+
+    await fetchPage(currentPage);
+  } catch (error) {
+    alert("Fetch error:", error.message);
+  }
+}
