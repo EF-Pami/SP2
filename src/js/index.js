8075d3d57b4adc6794a6b3e522c04c2dea2a8820
EF-Pami <pamijoelpami@gmail.com>
2024-06-23 21:19:09 +0200

    added index.js for the app

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const registerForm = document.querySelector("#registerForm");
+
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  registerUser();
+});
+
+function registerUser() {
+}
