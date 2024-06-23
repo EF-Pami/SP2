27ba04dd0130bd53d69d1754ef2437a2c66b03c1
EF-Pami <pamijoelpami@gmail.com>
2024-06-23 21:12:18 +0200

    added register.js file

---
+++ src/js/auth/register.js
@@ -0,0 +1,62 @@
+import { BASE_URL, REGISTER_ENDPOINT } from "../baseapi.js";
+
+export function register() {
+  document.addEventListener("DOMContentLoaded", function () {
+    document
+      .getElementById("registerForm")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const avatarUrlInput = document.getElementById("registerAvatar");
+        const avatarUrl = avatarUrlInput.value.trim();
+
+        const userData = {
+          name: document.getElementById("registerName").value,
+          email: document.getElementById("registerEmail").value,
+          password: document.getElementById("registerPassword").value,
+          bio: document.getElementById("registerBio").value,
+        };
+        
+        if (avatarUrl !== "") {
+          userData.avatar = {
+            url: avatarUrl,
+          };
+        }
+
+        fetch(BASE_URL + REGISTER_ENDPOINT, {
+          method: "POST",
+          body: JSON.stringify(userData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            if (response.status === 201) {
+              return response.json();
+            } else {
+              return response
+                .json()
+                .then((data) =>
+                  Promise.reject({ data, status: response.status }),
+                );
+            }
+          })
+          .then((data) => {
+            alert("User registered successfully!");
+          })
+          .catch(error => {
+            let errorMessage = "An unexpected error occurred. Please try again.";
+  
+            if (error.body && error.body.errors && error.body.errors.length > 0) {
+              errorMessage = error.body.errors[0].message;
+            } else if (error.message) {
+              errorMessage = error.message;
+            }
+            const loginErrorDiv = document.getElementById('registerError');
+            if (loginErrorDiv) {
+              loginErrorDiv.textContent = errorMessage;            
+            }
+          });
+      });
+  });
+}
