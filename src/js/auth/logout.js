bfe455aa84e3d026fe59a6a228ae2e12740a7054
EF-Pami <pamijoelpami@gmail.com>
2024-06-23 21:12:33 +0200

    added logout.js file

---
+++ src/js/auth/logout.js
@@ -0,0 +1,35 @@
+export function logout() {
+    const accessToken = localStorage.getItem("accessToken");  
+    if (accessToken) {
+      document.getElementById("logout-btn").classList.remove("d-none");
+    }
+  
+    document.getElementById("logout-btn").addEventListener("click", function () {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("userName");
+      localStorage.removeItem("profile");
+      this.classList.add("d-none");
+      window.location.href = "../index.html";
+    });
+  }
+  
+  export function manageLoginButton() {
+    const accessToken = localStorage.getItem("accessToken");
+    const loginButton = document.getElementById("login-btn");
+    const profile = document.getElementsByClassName("profile-circle");
+  
+    if (!accessToken) {
+      loginButton.classList.remove("d-none");
+      profile[0].classList.add("d-none");
+    } else if (loginButton) {
+      loginButton.classList.add("d-none");
+    }
+  
+      if (!accessToken && loginButton) {
+      loginButton.addEventListener("click", function () {      
+        window.location.href = "../index.html";
+      });
+    }
+}
+  
+  
